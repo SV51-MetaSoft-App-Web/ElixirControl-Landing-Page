c6a3d0698ef363310ca9a3c96d9d1ae658118f5b
Janover Saldaña <u20201b510@upc.edu.pe>
2024-09-05 01:01:15 -0500

    feat: Added header and navigation section.

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,20 @@
+const check = document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id = check.checked;
+
+    if( id === true){
+        location.href = "index.html"
+    }else{
+        location.href = "index-es.html"
+    }
+}
+
+function openNav(){
+    document.getElementById("mobile-menu") .style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu") .style.width = "0%";
+}
